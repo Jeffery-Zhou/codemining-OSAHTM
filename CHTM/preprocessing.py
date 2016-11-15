ad3270979a6f3dc7a2a13cd6f23a5100cae0ac7d
DaeShin <zhoupeng2014@gmail.com>
2016-11-15 20:47:19 +0800

    preprocessing editing

--- CHTM/preprocessing.py
+++ CHTM/preprocessing.py
@@ -1,41 +1,87 @@
 # coding:utf-8
 import codecs
+import nltk
+import gensim
+
 """
-this is data preprocessing for open source code: d3.v4.js
+this is data preprocessing for open source code: d3_3.5.17.js
 by Jeffery Zhou
-2016年11月11日
 """
 
 
 class Preprocess(object):
-    '''this is the data preprocessing class;
-
-    '''
-
-
+    """
+        this is the data preprocessing class;
+    """
 
     def __init__(self):
-        '''
+        """
 
-        '''
-        self.code_path = "/source_code/d3_test.js"
+        """
+        self.code_path = "source_code/d3_test.js"
         self.keyword_path = 'reserved_keywords.txt'
+        # self.code = self.get_code()
         self.stopword_path = '/'
 
     def get_keyword(self):
-        '''
-
-        :return:
-        '''
-        keywords_lst = []
+        """
+        get the keyword and reserved words in Javascript
+        :return: keywords_lst
+        """
         word_file = codecs.open(self.keyword_path, 'r')
         words = word_file.read()
         word_file.close()
         keywords_lst = words.split('\n')
         return keywords_lst
 
+    def get_code(self):
+        """ return the code according the code file path. """
+        code_file = codecs.open(self.code_path, 'r')
+        code = code_file.read()  # 注释的操作
+        code_file.close()
+        return code
+
+    def word_tokenize(self, code):
+        return nltk.word_tokenize(code)
+
+    def keep_word_combination(self, code):
+        pass
+
+    def remove_punctuation(self, code):
+        punc_lst = [
+            '[', ']', '!', '(', ')', '=', '{', '}', ':', ';', ',',
+            '&', '||', '--', '``', '+', '-', '*', '/', "''",
+            '>', '<', '<=', '>=', '==', '?', '===', '!'
+        ]
+        clean_code_lst = []
+        flag = True
+        for i in code:
+            for j in punc_lst:
+                if i == j:
+                    flag = False
+            if flag:
+                clean_code_lst.append(i)
+            flag = True
+        return clean_code_lst
+
+    def remove_keywords(self, code):
+        keywords = self.get_keyword()
+        clean_code_lst = []
+        for i in code:
+            for j in keywords:
+                if i == j:
+                    code.remove(i)
+        return code
+
+
+
 p = Preprocess()
-print p.get_keyword()
+code1 = p.word_tokenize(p.get_code())
+print code1
+code2 = p.remove_punctuation(code1)
+print code2
+code3 = p.remove_keywords(code2)
+print code3
 
 
 
--- CHTM/source_code/d3_test.js
+++ CHTM/source_code/d3_test.js
@@ -2,28 +2,31 @@
   var d3 = {
     version: "3.5.17"
   };
-  var d3_arraySlice = [].slice, d3_array = function(list) {
+  var d3_arraySlice = [].slice, d3_array = function (list) {
     return d3_arraySlice.call(list);
   };
   var d3_document = this.document;
+
   function d3_documentElement(node) {
     return node && (node.ownerDocument || node.document || node).documentElement;
   }
+
   function d3_window(node) {
     return node && (node.ownerDocument && node.ownerDocument.defaultView || node.document && node || node.defaultView);
   }
+
   if (d3_document) {
     try {
       d3_array(d3_document.documentElement.childNodes)[0].nodeType;
     } catch (e) {
-      d3_array = function(list) {
+      d3_array = function (list) {
         var i = list.length, array = new Array(i);
         while (i--) array[i] = list[i];
         return array;
       };
     }
   }
-  if (!Date.now) Date.now = function() {
+  if (!Date.now) Date.now = function () {
     return +new Date();
   };
   if (d3_document) {
@@ -31,13 +34,13 @@
       d3_document.createElement("DIV").style.setProperty("opacity", 0, "");
     } catch (error) {
       var d3_element_prototype = this.Element.prototype, d3_element_setAttribute = d3_element_prototype.setAttribute, d3_element_setAttributeNS = d3_element_prototype.setAttributeNS, d3_style_prototype = this.CSSStyleDeclaration.prototype, d3_style_setProperty = d3_style_prototype.setProperty;
-      d3_element_prototype.setAttribute = function(name, value) {
+      d3_element_prototype.setAttribute = function (name, value) {
         d3_element_setAttribute.call(this, name, value + "");
       };
-      d3_element_prototype.setAttributeNS = function(space, local, value) {
+      d3_element_prototype.setAttributeNS = function (space, local, value) {
         d3_element_setAttributeNS.call(this, space, local, value + "");
       };
-      d3_style_prototype.setProperty = function(name, value, priority) {
+      d3_style_prototype.setProperty = function (name, value, priority) {
         d3_style_setProperty.call(this, name, value + "", priority);
       };
     }
@@ -46,10 +49,11 @@
   function d3_ascending(a, b) {
     return a < b ? -1 : a > b ? 1 : a >= b ? 0 : NaN;
   }
-  d3.descending = function(a, b) {
+
+  d3.descending = function (a, b) {
     return b < a ? -1 : b > a ? 1 : b >= a ? 0 : NaN;
   };
-  d3.min = function(array, f) {
+  d3.min = function (array, f) {
     var i = -1, n = array.length, a, b;
     if (arguments.length === 1) {
       while (++i < n) if ((b = array[i]) != null && b >= b) {
@@ -66,7 +70,7 @@
     }
     return a;
   };
-  d3.max = function(array, f) {
+  d3.max = function (array, f) {
     var i = -1, n = array.length, a, b;
     if (arguments.length === 1) {
       while (++i < n) if ((b = array[i]) != null && b >= b) {
@@ -83,7 +87,7 @@
     }
     return a;
   };
-  d3.extent = function(array, f) {
+  d3.extent = function (array, f) {
     var i = -1, n = array.length, a, b, c;
     if (arguments.length === 1) {
       while (++i < n) if ((b = array[i]) != null && b >= b) {
@@ -104,15 +108,17 @@
         if (c < b) c = b;
       }
     }
-    return [ a, c ];
+    return [a, c];
   };
   function d3_number(x) {
     return x === null ? NaN : +x;
   }
+
   function d3_numeric(x) {
     return !isNaN(x);
   }
-  d3.sum = function(array, f) {
+
+  d3.sum = function (array, f) {
     var s = 0, n = array.length, a, i = -1;
     if (arguments.length === 1) {
       while (++i < n) if (d3_numeric(a = +array[i])) s += a;
@@ -121,7 +127,7 @@
     }
     return s;
   };
-  d3.mean = function(array, f) {
+  d3.mean = function (array, f) {
     var s = 0, n = array.length, a, i = -1, j = n;
     if (arguments.length === 1) {
       while (++i < n) if (d3_numeric(a = d3_number(array[i]))) s += a; else --j;
@@ -130,11 +136,11 @@
     }
     if (j) return s / j;
   };
-  d3.quantile = function(values, p) {
+  d3.quantile = function (values, p) {
     var H = (values.length - 1) * p + 1, h = Math.floor(H), v = +values[h - 1], e = H - h;
     return e ? v + e * (values[h] - v) : v;
   };
-  d3.median = function(array, f) {
+  d3.median = function (array, f) {
     var numbers = [], n = array.length, a, i = -1;
     if (arguments.length === 1) {
       while (++i < n) if (d3_numeric(a = d3_number(array[i]))) numbers.push(a);
@@ -143,7 +149,7 @@
     }
     if (numbers.length) return d3.quantile(numbers.sort(d3_ascending), .5);
   };
-  d3.variance = function(array, f) {
+  d3.variance = function (array, f) {
     var n = array.length, m = 0, a, d, s = 0, i = -1, j = 0;
     if (arguments.length === 1) {
       while (++i < n) {
@@ -164,13 +170,13 @@
     }
     if (j > 1) return s / (j - 1);
   };
-  d3.deviation = function() {
+  d3.deviation = function () {
     var v = d3.variance.apply(this, arguments);
     return v ? Math.sqrt(v) : v;
   };
   function d3_bisector(compare) {
     return {
-      left: function(a, x, lo, hi) {
+      left: function (a, x, lo, hi) {
         if (arguments.length < 3) lo = 0;
         if (arguments.length < 4) hi = a.length;
         while (lo < hi) {
@@ -179,7 +185,7 @@
         }
         return lo;
       },
-      right: function(a, x, lo, hi) {
+      right: function (a, x, lo, hi) {
         if (arguments.length < 3) lo = 0;
         if (arguments.length < 4) hi = a.length;
         while (lo < hi) {
@@ -190,15 +196,16 @@
       }
     };
   }
+
   var d3_bisect = d3_bisector(d3_ascending);
   d3.bisectLeft = d3_bisect.left;
   d3.bisect = d3.bisectRight = d3_bisect.right;
-  d3.bisector = function(f) {
-    return d3_bisector(f.length === 1 ? function(d, x) {
+  d3.bisector = function (f) {
+    return d3_bisector(f.length === 1 ? function (d, x) {
       return d3_ascending(f(d), x);
     } : f);
   };
-  d3.shuffle = function(array, i0, i1) {
+  d3.shuffle = function (array, i0, i1) {
     if ((m = arguments.length) < 3) {
       i1 = array.length;
       if (m < 2) i0 = 0;
@@ -210,20 +217,20 @@
     }
     return array;
   };
-  d3.permute = function(array, indexes) {
+  d3.permute = function (array, indexes) {
     var i = indexes.length, permutes = new Array(i);
     while (i--) permutes[i] = array[indexes[i]];
     return permutes;
   };
-  d3.pairs = function(array) {
+  d3.pairs = function (array) {
     var i = 0, n = array.length - 1, p0, p1 = array[0], pairs = new Array(n < 0 ? 0 : n);
-    while (i < n) pairs[i] = [ p0 = p1, p1 = array[++i] ];
+    while (i < n) pairs[i] = [p0 = p1, p1 = array[++i]];
     return pairs;
   };
-  d3.transpose = function(matrix) {
+  d3.transpose = function (matrix) {
     if (!(n = matrix.length)) return [];
-    for (var i = -1, m = d3.min(matrix, d3_transposeLength), transpose = new Array(m); ++i < m; ) {
-      for (var j = -1, n, row = transpose[i] = new Array(n); ++j < n; ) {
+    for (var i = -1, m = d3.min(matrix, d3_transposeLength), transpose = new Array(m); ++i < m;) {
+      for (var j = -1, n, row = transpose[i] = new Array(n); ++j < n;) {
         row[j] = matrix[j][i];
       }
     }
